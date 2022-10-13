1d8b6aafef4aeb7da5c321c1d66229ee599f986e
hseonkang-MD <hseonkang-md@hseonkang-MDui-Mac-Studio.local>
2022-10-13 14:01:30 +0900

    update micro task

--- src/section-one/micro-macro-task.ts
+++ src/section-one/micro-macro-task.ts
@@ -24,16 +24,8 @@ export const excuteMicroMacroTask = () => {
     Promise.resolve().then(() => promiseCallback());
 };
 
-// TODO: 콘솔의 표시를 텍스트로 바꾸어 html 템플릿의 result 영역에 표시하세요.
-export const questionMicroMacroTask = () => {
-    const firstTask = () => console.log('first');
-    const secondTask = () => console.log('second');
-    const thirdTask = () => Promise.resolve().then(() => console.log('third'));
-    const fourthTask = () => setTimeout(() => console.log('fourth'), 0);
-};
-
 // TODO: 콘솔에 onUpdate for First -> second -> third 순으로 출력하세요.
-export const questionMicroMacroTaskSecond = () => {
+export const questionMicroMacroTask = () => {
     const caller: TaskCaller = new TaskCaller();
     caller.onUpdate = () => {
         console.log('onUpdate for First');

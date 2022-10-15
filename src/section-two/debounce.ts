44e912ef7c6f2b682ac68e7f172110583e9bcd6e
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2022-10-16 00:02:48 +0900

    added section two (closure, debounce)

---
+++ src/section-two/debounce.ts
@@ -0,0 +1,15 @@
+export const debounce = (callback: Function, delayTime = 0) => {
+    let timeout: any = null;
+    return (...args: any[]) => {
+        // 실행되지 않은 settimeout은 clear
+        if (timeout) clearTimeout(timeout);
+
+        // settimeout clear를 위해 저장.
+        timeout = setTimeout(() => {
+            // 지정된 함수 실행
+            callback(...args);
+            // 함수 실행 후 settimeout clear
+            clearTimeout(timeout);
+        }, delayTime);
+    }
+}

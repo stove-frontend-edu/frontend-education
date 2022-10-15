61e4b6fb80996c5adfad4e4e87dcd6fe2a569dab
강효선 <kanghyoseon@ganghyoseon-ui-MacBook-Pro.local>
2022-10-16 00:25:15 +0900

    modify questin( added closure state and debounce question)

---
+++ src/section-two/debounce.ts
@@ -0,0 +1,7 @@
+export const debounce = (callback: Function, delayTime = 0) => {
+    // @TODO: debounce 기능을 구현하세요.
+    // 조건
+    // 1. 함수를 return 해야함.
+    // 2. settimeout 활용하도록 함.
+    // 3. clearTimeout을 반드시 해줄 것.
+};

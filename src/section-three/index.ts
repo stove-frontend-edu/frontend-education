039215d26475d109093465d541c8120de6633dfa
hseonkang-MD <hseonkang-md@hseonkang-MDui-Mac-Studio.local>
2022-10-25 13:52:37 +0900

    modify question

--- src/section-three/index.ts
+++ src/section-three/index.ts
@@ -15,4 +15,5 @@ export const execAutoComplete = () => {
     container?.appendChild(textInput);
     // TODO: fromEvent를 사용하여 text input 의 keyup 이벤트 처리 후
     // debounceTime, operator를 사용하여 자동완성 기능을 구현하세요.
+    // fromEvent 참고: https://rxjs.dev/api/index/function/fromEvent
 };

b9d427a558f4bb6a66592a8cdee2474eb86d51f8
hseonkang-MD <hseonkang-md@hseonkang-MDui-Mac-Studio.local>
2022-10-25 11:31:48 +0900

    added section three

---
+++ src/section-three/index.ts
@@ -0,0 +1,18 @@
+import { fromEvent, Subject, of, fromEventPattern } from 'rxjs';
+import {
+    map,
+    filter,
+    debounceTime,
+    distinctUntilChanged,
+} from 'rxjs/operators';
+
+export const execAutoComplete = () => {
+    const container = document.querySelector('#result');
+    const textInput = document.createElement('INPUT');
+    textInput.setAttribute('type', 'text');
+    textInput.setAttribute('id', 'txtInput');
+    textInput.style.cssText = `width: 300px`;
+    container?.appendChild(textInput);
+    // TODO: fromEvent를 사용하여 text input 의 keyup 이벤트 처리 후
+    // debounceTime, operator를 사용하여 자동완성 기능을 구현하세요.
+};
